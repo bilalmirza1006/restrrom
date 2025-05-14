1967b5242445fc2e92b10026d7cf9b4e38ae425e
bilaltetraintech <bilal@tetraintech.com>
2025-05-14 11:09:06 +0500

    fix the routing on the role base

--- src/components/user/buildings/AllBuildings.jsx
+++ src/components/user/buildings/AllBuildings.jsx
@@ -1,23 +1,46 @@
+'use client';
 import BuildingCard from '@/components/global/BuildingCard';
 import { buildingData } from '@/data/data';
 import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 const AllBuildings = () => {
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+  console.log('user', user.role);
+  const getRouteByRole = (role, id) => {
+    switch (role) {
+      case 'admin':
+        return `/admin/buildings/building-details/${id}`;
+      case 'user':
+        return `/buildings/building-detail/${id}`;
+      default:
+        return '';
+    }
+  };
+
   return (
     <div className="bg-white rounded-2xl shadow-md p-4 md:p-5">
       <div className="mb-4 flex justify-between items-center">
         <h4 className="text-base md:text-lg font-semibold leading-[32px]">All Buildings</h4>
-        <Link href="/add-building">
-          <FaPlus className="text-blue-500 hover:text-blue-600 text-2xl" />
-        </Link>
+        {user.role === 'user' && (
+          <Link href="/add-building">
+            <FaPlus className="text-blue-500 hover:text-blue-600 text-2xl" />
+          </Link>
+        )}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
-        {buildingData.map((building, i) => (
-          <Link href={`/buildings/building-detail/${building?.id}`} key={i}>
-            <BuildingCard data={building} />
-          </Link>
-        ))}
+        {buildingData.map((building, i) => {
+          const route = getRouteByRole(user?.role, building?.id);
+
+          return route ? (
+            <Link href={route} key={i}>
+              <BuildingCard data={building} />
+            </Link>
+          ) : (
+            <BuildingCard data={building} key={i} />
+          );
+        })}
       </div>
     </div>
   );
