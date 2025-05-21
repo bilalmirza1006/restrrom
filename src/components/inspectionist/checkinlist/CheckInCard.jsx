bff27ad1d90c567d61797b6a72adacf562b8c484
Muhammad Zain <zain.ashiq613@gmail.com>
2025-05-21 17:28:47 +0500

    changes to the inspectionist building details page

--- src/components/inspectionist/checkinlist/CheckInCard.jsx
+++ src/components/inspectionist/checkinlist/CheckInCard.jsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { inspectionistFloorListData } from "@/data/data";
-import { useGetAllRestroomsQuery } from "@/features/restroom/restroomApi";
-import Image from "next/image";
-import { useState } from "react";
-import { FaCaretDown } from "react-icons/fa";
-import InputFields from "./AddFields";
-import InspectionFields from "./InspectionFields";
+import { inspectionistFloorListData } from '@/data/data';
+import { useGetAllRestroomsQuery } from '@/features/restroom/restroomApi';
+import Image from 'next/image';
+import { useState } from 'react';
+import { FaCaretDown } from 'react-icons/fa';
+import InputFields from './AddFields';
+import InspectionFields from './InspectionFields';
 
 const CheckInCard = ({ buildingId }) => {
   const { data } = useGetAllRestroomsQuery(buildingId);
@@ -18,10 +18,13 @@ const CheckInCard = ({ buildingId }) => {
       <div className="mt-3 flex flex-col gap-4">
         {data?.data?.map((restroom) => {
           return (
-            <div key={restroom?._id} className="bg-white shadow-sm rounded-lg flex flex-col">
+            <div
+              key={restroom?._id}
+              className="bg-white overflow-auto w-[700px] sm:w-full shadow-sm rounded-lg flex flex-col"
+            >
               <div className="flex items-center justify-between py-2 px-3">
                 <div className="flex items-center gap-3">
-                  <Image src={"/svgs/user/total-restrooms.svg"} width={30} height={34} alt="icon" />
+                  <Image src={'/svgs/user/total-restrooms.svg'} width={30} height={34} alt="icon" />
                   <h1 className="text-[#05004E] text-[20px] font-semibold">{restroom?.name}</h1>
                 </div>
 
@@ -29,20 +32,24 @@ const CheckInCard = ({ buildingId }) => {
                   <>
                     <div className="text-center">
                       <span className="block text-[#05004E80] text-[12px]">Type</span>
-                      <h1 className="text-[#A449EB] text-[16px] font-semibold capitalize">{restroom?.type}</h1>
+                      <h1 className="text-[#A449EB] text-[16px] font-semibold capitalize">
+                        {restroom?.type}
+                      </h1>
                     </div>
 
                     <div className="text-center">
                       <span className="block text-[#05004E80] text-[12px]">Number of Toilets</span>
-                      <h1 className="text-[#A449EB] text-[16px] font-semibold">{restroom?.numOfToilets}</h1>
+                      <h1 className="text-[#A449EB] text-[16px] font-semibold">
+                        {restroom?.numOfToilets}
+                      </h1>
                     </div>
                   </>
                 )}
 
                 <div className="flex items-center gap-4">
                   <p
-                    className={`inline-block px-4 py-1.5 rounded-[8px] text-white ${
-                      restroom?.status === "Active" ? "bg-[#61CA94]" : "bg-[#FF8080]"
+                    className={`inline-block capitalize px-4 py-1.5 rounded-[8px] text-white ${
+                      restroom?.status === 'Active' || 'active' ? 'bg-[#61CA94]' : 'bg-[#FF8080]'
                     }`}
                   >
                     {restroom?.status}
@@ -54,8 +61,8 @@ const CheckInCard = ({ buildingId }) => {
                     <FaCaretDown
                       className={
                         tableId === restroom._id
-                          ? "ease-in transition-transform duration-300 transform  rotate-180"
-                          : "transition-transform duration-300"
+                          ? 'ease-in transition-transform duration-300 transform  rotate-180'
+                          : 'transition-transform duration-300'
                       }
                       fill="#A449EB96"
                     />
--- src/components/inspectionist/checkinlist/CheckInList.jsx
+++ src/components/inspectionist/checkinlist/CheckInList.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import CheckInCard from "./CheckInCard";
+import React from 'react';
+import CheckInCard from './CheckInCard';
 
 function CheckInList({ buildingId }) {
   return (
     <div>
       <h1 className="text-[#05004E] text-[20px] font-semibold">All Restrooms</h1>
-      <CheckInCard buildingId={buildingId} />
+      <div className="w-[720px] sm:w-full overflow-auto">
+        <CheckInCard buildingId={buildingId} />
+      </div>
     </div>
   );
 }
