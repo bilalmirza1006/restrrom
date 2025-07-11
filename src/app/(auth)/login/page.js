ccafdc282cb5151cca76b05ee538e01e03798c8e
muhammadnisarbsse7 <muhammadnisarbsse7@gmail.com>
2025-07-11 16:02:52 +0500

    hell

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -4,7 +4,7 @@ import LoginForm from "@/components/auth/LoginForm";
 const Login = () => {
   return (
     <Layout
-      title="Welcome to Restroom"
+      title="Welcome to Restrooms"
       secondTitle="Real-time insights for real-time care. Sensors that track and enhance hygiene. Efficient management at your fingertips.
 "
     >
